6d9a7d882f44fde6b724b3c75394c331a1a073a0
Maxim Ostanin <ostanin.maks@gmail.com>
2023-07-03 19:23:46 +0300

    firsr commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    @Autowired
+    public Needle7(Deth8 death) {
+
+    }
+
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( ";
     }
 }
