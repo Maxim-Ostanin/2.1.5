edc50e40b3677c82edf35fb561d75cfa3425a366
Maxim Ostanin <ostanin.maks@gmail.com>
2023-07-05 22:22:51 +0300

    implement field 8

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
+    private final Deth8 death;
 
     @Autowired
     public Needle7(Deth8 death) {
-
+        this.death = death;
     }
 
 
